d0c696aec6c4fb776595b1cceb8ded1ac26a45dc
Peter Frederick Robinson <peter.frederick.robinson@gmail.com>
2019-11-08 12:18:37 +1300

    scorecard created in top left corner - not linked to onclick

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+// import Button from 'react-bootstrap/Button'
 
 class Counter extends React.Component {
   state = {
@@ -12,13 +14,29 @@ class Counter extends React.Component {
     }))
   }
 
+  //   render () {
+  //     return (
+  //       <React.Fragment>
+  //         <div>
+  //           <div onClick={this.handleClick}>Your Score</div>
+  //           <button>{this.state.count}</button>
+  //         </div>
+
+  //       </React.Fragment>
+  //     )
+  //   }
+  // }
+
   render () {
     return (
       <React.Fragment>
-        <div>
-          <button onClick={this.handleClick}>Wally</button>
-          <button>{this.state.count}</button>
-        </div>
+      
+        <Card className="card text-white bg-dark mb-3" style={{ width: '15rem', height: '7rem' }}>
+          <Card.Body>
+            <Card.Title>Dons Found</Card.Title>
+            <Card.Title>{this.state.count}</Card.Title>
+          </Card.Body>
+        </Card>
 
       </React.Fragment>
     )
