5f233378d3c783eb821edf1818ad2f2130578f36
Sunny Niu <sunnyyingli@gmail.com>
2019-11-08 11:32:07 +1300

    add random position

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+function getRandomInt (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min // The maximum is exclusive and the minimum is inclusive
+}
+
+function getWidth () {
+  const width = window.innerWidth
+  const randomWidth = getRandomInt(100, width - 100)
+
+  return randomWidth
+}
+
+function getHeight () {
+  const height = window.innerHeight
+  const randomHeight = getRandomInt(200, height - 200)
+  return randomHeight
+}
+
+class App extends React.Component {
+  state = {
+    circle: {
+      cx: getWidth(),
+      cy: getHeight(),
+      r: 40
+    },
+    count: 0
+  }
+
+  render () {
+    return (
+      <svg width={window.innerWidth} height={window.innerHeight}>
+        <circle cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
+      </svg>
+    )
+  }
 }
 
 export default App
