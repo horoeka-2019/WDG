9c15ead59fb6cc3bfcd1b532c0efc7792892c467
Sunny Niu <sunnyyingli@gmail.com>
2019-11-08 14:57:20 +1300

    fix conflict

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -44,27 +44,27 @@ class App extends React.Component {
     }
     return (
       <React.Fragment>
-<<<<<<< HEAD
         <Counter />
-        <svg width={window.innerWidth} height={window.innerHeight} style={styles.paperContainer}>
-          <defs>
-            <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
-              <image x="0" y="0" xlinkHref="/images/don_wally.jpeg"></image>
-            </pattern>
-          </defs>
-          <circle fill="url(#image)" cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
-        </svg>
-=======
+ 
         <Landing />
         <Game handleClick={this.handleClick} count={this.state.count}/>
 
         <Route exact path='/' component = { Landing } />
         <Route path='/game' component = { Don } />
 
->>>>>>> 42a66c72dfb795985215ba33a2b6471c83850212
       </React.Fragment>
     )
   }
 }
+
+
+{/* <svg width={window.innerWidth} height={window.innerHeight} style={styles.paperContainer}>
+<defs>
+  <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+    <image x="0" y="0" xlinkHref="/images/don_wally.jpeg"></image>
+  </pattern>
+</defs>
+<circle fill="url(#image)" cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
+</svg> */}
 
 export default App
