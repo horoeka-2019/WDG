b8d2ab535dd3c506d2433b2c86172d144f705e50
Sunny Niu <sunnyyingli@gmail.com>
2019-11-08 14:52:30 +1300

    add pattern

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -20,6 +20,9 @@ function getHeight () {
   return randomHeight
 }
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
 class App extends React.Component {
   state = {
     circle: {
@@ -27,15 +30,32 @@ class App extends React.Component {
       cy: getHeight(),
       r: 40
     },
-    count: 0
+    count: 0,
+    color: randomHexColor()
   }
 
   render () {
+    const styles = {
+      paperContainer: {
+        backgroundImage: `url(${'images/wally1.jpg'})`
+      }
+    }
+
+    const donStyle = {
+      container: {
+        fill: `url(${'images/don_wally.jpg'})`
+      }
+    }
     return (
       <React.Fragment>
         <Counter />
-        <svg width={window.innerWidth} height={window.innerHeight}>
-          <circle cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
+        <svg width={window.innerWidth} height={window.innerHeight} style={styles.paperContainer}>
+          <defs>
+            <pattern id="image" x="0" y="0" patternUnits="userSpaceOnUse" height="100%" width="100%">
+              <image x="0" y="0" xlinkHref="/images/don_wally.jpeg"></image>
+            </pattern>
+          </defs>
+          <circle fill="url(#image)" cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
         </svg>
       </React.Fragment>
     )
