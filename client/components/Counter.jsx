9a3000c538407d43d326181f85ab959feef00044
Peter Frederick Robinson <peter.frederick.robinson@gmail.com>
2019-11-08 13:09:32 +1300

    counter is now a header full width with count and title

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -18,7 +18,7 @@ class Counter extends React.Component {
   //     return (
   //       <React.Fragment>
   //         <div>
-  //           <div onClick={this.handleClick}>Your Score</div>
+  //           <div onClick={this.handleClick}></div>
   //           <button>{this.state.count}</button>
   //         </div>
 
@@ -30,14 +30,15 @@ class Counter extends React.Component {
   render () {
     return (
       <React.Fragment>
-      
-        <Card className="card text-white bg-dark mb-3" style={{ width: '15rem', height: '7rem' }}>
+
+        <Card className="text-center card text-black bg-danger mb-3" style={{ width: '100%', height: '10rem' }}>
           <Card.Body>
-            <Card.Title>Dons Found</Card.Title>
-            <Card.Title>{this.state.count}</Card.Title>
+            <Card.Title className='font-weight-bold' style={{ fontSize: '2rem' }}>WHERE HAS DON GON?!</Card.Title>
+            <Card.Subtitle style={{ fontSize: '1.3rem' }}>Your Score:</Card.Subtitle>
+            <br></br>
+            <Card.Subtitle style={{ fontSize: '1.3rem' }}>{this.state.count}</Card.Subtitle>
           </Card.Body>
         </Card>
-
       </React.Fragment>
     )
   }
