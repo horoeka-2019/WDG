42a66c72dfb795985215ba33a2b6471c83850212
EDA <marinaslussarek@gmail.com>
2019-11-08 14:51:10 +1300

    import Game.jsx to App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-//import Counter from './Counter'
+// import Counter from './Counter'
 import Don from './Don'
 import Landing from './Landing'
 import { Route } from 'react-router-dom'
+import Game from './Game'
 
 class App extends React.Component {
   state = {
@@ -20,17 +21,10 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Landing />
-        <Counter count={this.state.count}/>
-        <Don handleClick={this.handleClick} count={this.state.count}/>
-        {/* <Don /> */}
+        <Game handleClick={this.handleClick} count={this.state.count}/>
+
         <Route exact path='/' component = { Landing } />
         <Route path='/game' component = { Don } />
-        {/* <Counter />
-        <svg width={window.innerWidth} height={window.innerHeight}>
-          <circle cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
-        </svg> */}
-        {/* <Landing />
-        <Counter /> */}
 
       </React.Fragment>
     )
